982aae8235d1a532c86853f495d004e2e76abb9b
ElTurbinas <73681786+ElTurbinas@users.noreply.github.com>
2022-11-06 20:18:46 +0000

    Web Update 2

--- replit.nix
+++ replit.nix
@@ -3,5 +3,10 @@
         pkgs.yarn
         pkgs.cowsay
 		pkgs.nodejs-16_x
+		pkgs.nodePackages.typescript-language-server
+		pkgs.nodePackages.node-pre-gyp
+		pkgs.libpng
+		pkgs.libjpeg
+		pkgs.libuuid
     ];
 }
--- src/Routers/public/index.js
+++ src/Routers/public/index.js
@@ -4,11 +4,20 @@ const path = require('path');
 
 var PageRouter = Router()
 
+PageRouter.get('/support', async (_, res) => {
+	res.redirect("https://discord.gg/kRWvJJBpZd")
+})
+
+PageRouter.get('/invite', async (_, res) => {
+	res.redirect("https://discord.com/api/oauth2/authorize?client_id=978037412078964847&permissions=8&scope=bot%20applications.commands")
+})
+
 PageRouter.get('/', async (_, res) => {
     const botinfo = await getInfo()
     res.status(200).render("index.ejs", {botinfo})
     console.log("[WEB] Se renderizo index.ejs")
 })
+
 
 PageRouter.get("/assets/:subdir/:filename", (req, res) => {
     res.sendFile(path.join(__dirname, `../../assets/${req.params.subdir}/${req.params.filename}`))
